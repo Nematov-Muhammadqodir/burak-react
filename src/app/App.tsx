b257baee3fb9eb723924426d5ebedfbe54bf9371
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-04-14 17:32:34 +0900

    feat: MUI standard integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import "../css/app.css";
+import { Stack, Container, Box } from "@mui/material";
+import Button from "@mui/material/Button";
 
 function App() {
-  return <div>HOME</div>;
+  return (
+    <Container maxWidth="xs">
+      <Stack spacing={2}>
+        <Box
+          component="section"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            p: 2,
+            border: "1px dashed grey",
+            gap: 2,
+          }}
+        >
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Box>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
-
-
-
-
-
