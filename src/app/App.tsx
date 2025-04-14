f3e17224110a6dcf484abd1c323637cb18945f57
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-04-14 18:26:48 +0900

    fix: MUI customized integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import "../css/app.css";
-import { Stack, Container, Box } from "@mui/material";
+import { Stack, Container, Box, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
+import { RippleBadge } from "./MaterialTheme/styled";
 
 function App() {
   return (
-    <Container maxWidth="xs">
-      <Stack spacing={2}>
-        <Box
-          component="section"
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            p: 2,
-            border: "1px dashed grey",
-            gap: 2,
-          }}
-        >
-          <Button variant="text">Text</Button>
-          <Button variant="contained">Contained</Button>
-          <Button variant="outlined">Outlined</Button>
+    <Container sx={{ background: "orange" }}>
+      <Stack flexDirection={"column"}>
+        <Box sx={{ my: 4 }}>
+          <Typography component={"h2"} align="center">
+            These are MUI Button components
+          </Typography>
+        </Box>
+        <Box>
+          <RippleBadge badgeContent={4}>
+            <Button variant="contained">Contained</Button>
+          </RippleBadge>
         </Box>
       </Stack>
     </Container>
