141b44c43bcb9a053318316bb8096b1b808cf198
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-04-18 12:57:08 +0900

    fix: organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,40 +3,23 @@ import "../css/app.css";
 import { Stack, Container, Box, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import { RippleBadge } from "./MaterialTheme/styled";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
 import { HomePage } from "./screens/homePage";
 import { UsersPage } from "./screens/userPage";
 import { ProductsPage } from "./screens/productsPage";
 import { AboutPage } from "./screens/aboutPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { HelpPage } from "./screens/helpPage";
+import { HomeNavbar } from "./components/headers/HomeNavbar";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import { Footer } from "./components/footer";
 
 function App() {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">HomePage</Link>
-          </li>
-          <li>
-            <Link to="/products">ProductsPage</Link>
-          </li>
-          <li>
-            <Link to="/orders">OrdersPage</Link>
-          </li>
-          <li>
-            <Link to="/member-page">UserPage</Link>
-          </li>
-          {/* <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/help">HelpPage</Link>
-          </li> */}
-        </ul>
-      </nav>
+  const location = useLocation();
 
+  return (
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
       <Switch>
         <Route path="/products">
           <ProductsPage />
@@ -53,15 +36,9 @@ function App() {
         <Route path="/">
           <HomePage />
         </Route>
-
-        {/* <Route path="/about">
-          <AboutPage />
-        </Route>
-        <Route path="/help">
-          <HelpPage />
-        </Route> */}
       </Switch>
-    </div>
+      <Footer />
+    </>
   );
 }
 
