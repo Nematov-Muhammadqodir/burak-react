ccda49fff41c2fabecdf4329b9889fa8a49c5ff8
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-04-19 22:26:30 +0900

    fix: organize home page related sectional components

---
+++ src/app/screens/homePage/PopularDishes.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+export default function PopularDishes() {
+  return (
+    <div>
+      <Container>Popular Dishes</Container>
+    </div>
+  );
+}
